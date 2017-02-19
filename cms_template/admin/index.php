1382c39fb07a5dd3efb51d7e40eb581341c707f4
Manjunath Ravi <mravi@softlayer.com>
2017-02-19 15:47:03 -0600

    Initial commit for cms_template project

---
+++ cms_template/admin/index.php
@@ -0,0 +1,17 @@
+<?php include("includes/header.php"); ?>
+
+        <!-- Navigation -->
+            <!-- Top Menu Items -->
+            <?php include("includes/top_nav.php"); ?>
+            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+            <?php include("includes/side_nav.php"); ?>
+            <!-- /.navbar-collapse -->
+
+        <div id="page-wrapper">
+
+            <?php include("includes/admin_content.php"); ?>
+
+        </div>
+        <!-- /#page-wrapper -->
+
+  <?php include("includes/footer.php"); ?>
